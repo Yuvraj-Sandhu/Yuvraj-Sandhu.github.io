812709a96aa95a3d760b59dcc23c3e7cc953a71c
Yuvraj Sandhu <103527950+Yuvraj-Sandhu@users.noreply.github.com>
2023-10-26 19:44:13 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Yuvraj-Sandhu.github.io',
+  site: 'https://yuvrajsandhu.com',
   integrations: [react(), tailwind()],
   vite: {
     ssr: {
